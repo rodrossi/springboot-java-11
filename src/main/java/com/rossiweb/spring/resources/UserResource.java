01910c860e88ba07d7496f51885e25b76299f26d
Rodrigo Rossi <rodrigoormonde@gmail.com>
2020-07-31 11:43:44 -0300

    User insert

--- src/main/java/com/rossiweb/spring/resources/UserResource.java
+++ src/main/java/com/rossiweb/spring/resources/UserResource.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,5 +32,11 @@ public class UserResource {
 	User obj = service.finById(id);
 	return ResponseEntity.ok().body(obj);
 	
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		obj = service.insert(obj);
+		return ResponseEntity.ok().body(obj);
 	}
 }
